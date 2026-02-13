bf1c1c1f31c356e683fcb6626ddaf432e5a058ac
aloon <118343415+aelaen-1@users.noreply.github.com>
2026-02-13 20:34:04 +0100

    format Parcours

--- src/main/java/fr/miage/projetjava/Parcours.java
+++ src/main/java/fr/miage/projetjava/Parcours.java
@@ -3,12 +3,11 @@ package fr.miage.projetjava;
 import java.util.ArrayList;
 
 public class Parcours {
-    private int Id;
     private String nom;
     private ArrayList<UE> UEObligatoire = new ArrayList<>();
     private Mention mention;
 
-    public Parcours(String nom, Mention mention){
+    public Parcours(String nom, Mention mention) {
         this.nom = nom;
         this.mention = mention;
     }
@@ -21,10 +20,8 @@ public class Parcours {
         return nom;
     }
 
-    public void addUEObligatoire(UE ue){
-        if(!UEObligatoire.contains(ue)){
+    public void addUEObligatoire(UE ue) {
+        if (!UEObligatoire.contains(ue))
             UEObligatoire.add(ue);
-        }
-
     }
 }
