c6166875030dfb1a1b4bdf7e7d9ff6379fb4cf8c
aloon <118343415+aelaen-1@users.noreply.github.com>
2026-02-13 20:45:35 +0100

    classe ScolariteService formatée, refacto et fonctionnelle

--- src/main/java/fr/miage/projetjava/ScolariteService.java
+++ src/main/java/fr/miage/projetjava/ScolariteService.java
@@ -4,48 +4,50 @@ import java.util.ArrayList;
 
 public class ScolariteService {
 
-    public void remplirListeUEValidees(ArrayList<UE> UEvalidees, ArrayList<ResultatUE> resultats, int totalEcts){
-        
-        for (int i = 0; i < resultats.size(); i++){
+    public int remplirListeUEValidees(ArrayList<UE> UEvalidees, ArrayList<ResultatUE> resultats) {
+
+        int totalEcts = 0;
+
+        for (int i = 0; i < resultats.size(); i++) {
             if (resultats.get(i).getStatut() == StatutUE.VALIDE) {
-                if (totalEcts > -1)
-                    totalEcts += resultats.get(i).getUe().getCredit();
+                totalEcts += resultats.get(i).getUe().getCredit();
                 UEvalidees.add(resultats.get(i).getUe());
             }
         }
+        return totalEcts;
     }
 
     // vérifie que les UE obligatoires/pré-requises ont été validées par l'étudiant
-    public Boolean verifierValidationDesUEObligatoires(ArrayList<UE> UEvalidees, ArrayList<UE> UEObligatoires){
+    public Boolean verifierValidationDesUEObligatoires(ArrayList<UE> UEvalidees, ArrayList<UE> UEObligatoires) {
 
-        while (!UEObligatoires.isEmpty()){
-            if (UEvalidees.contains(UEObligatoires.getFirst()))
-                UEObligatoires.removeFirst();
+        ArrayList<UE> tempList = new ArrayList<>(UEObligatoires);
+        while (!tempList.isEmpty()) {
+            if (UEvalidees.contains(tempList.getFirst()))
+                tempList.removeFirst();
             else
                 return false;
         }
         return true;
     }
 
-    public Boolean verifierValidationPrerequis(UE ue, Etudiant etudiant){
+    public Boolean verifierValidationPrerequis(UE ue, Etudiant etudiant) {
 
         ArrayList<ResultatUE> resultats = etudiant.getResultatsUE();
         ArrayList<UE> UEvalidees = new ArrayList<>();
-        remplirListeUEValidees(UEvalidees, resultats, -1);
-        
-        ArrayList<UE> UEprerequis = ue.getUEprerequis();
+        remplirListeUEValidees(UEvalidees, resultats);
+
+        ArrayList<UE> UEprerequis = new ArrayList<>(ue.getUEprerequis());
         return verifierValidationDesUEObligatoires(UEvalidees, UEprerequis);
     }
 
-    public Boolean estDiplome(Etudiant etudiant){
+    public Boolean estDiplome(Etudiant etudiant) {
 
         ArrayList<ResultatUE> resultats = etudiant.getResultatsUE();
         ArrayList<UE> UEObligatoires = etudiant.getParcours().getUEObligatoire();
         ArrayList<UE> UEvalidees = new ArrayList<>();
-        int totalEcts = 0;
 
-        remplirListeUEValidees(UEvalidees, resultats, totalEcts);
-        return ((totalEcts >= 180) && verifierValidationDesUEObligatoires(UEvalidees, UEObligatoires));
+        int totalEcts = remplirListeUEValidees(UEvalidees, resultats);
+        return ((totalEcts >= 180) && verifierValidationDesUEObligatoires(UEvalidees,
+                UEObligatoires));
     }
 }
-
