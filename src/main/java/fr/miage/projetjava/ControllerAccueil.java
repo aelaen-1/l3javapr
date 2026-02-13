2e2dfbb1a38d2a2e46df4702c84c5806bed8491f
Anais <anaistab7@gmail.com>
2026-02-13 21:59:18 +0100

    création du fichier accueil.fxml et de son controller ControllerAccueil.java

---
+++ src/main/java/fr/miage/projetjava/ControllerAccueil.java
@@ -0,0 +1,12 @@
+package fr.miage.projetjava;
+
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+
+public class ControllerAccueil {
+    @FXML
+    private void afficheEtudiant(ActionEvent event)
+    {
+
+    }
+}
