4982688c74aada15bea8afe099afab252f15e07f
aloon <118343415+aelaen-1@users.noreply.github.com>
2026-02-15 00:16:45 +0100

    ajout des packages MCV et metier pour la     structure du projet

---
+++ src/main/java/fr/miage/projetjava/metier/ScolariteService.java
@@ -0,0 +1,54 @@
+package fr.miage.projetjava.metier;
+
+import java.util.ArrayList;
+import fr.miage.projetjava.model.*;
+
+public class ScolariteService {
+
+    public int remplirListeUEValidees(ArrayList<UE> UEvalidees, ArrayList<ResultatUE> resultats) {
+
+        int totalEcts = 0;
+
+        for (int i = 0; i < resultats.size(); i++) {
+            if (resultats.get(i).getStatut() == StatutUE.VALIDE) {
+                totalEcts += resultats.get(i).getUe().getCredit();
+                UEvalidees.add(resultats.get(i).getUe());
+            }
+        }
+        return totalEcts;
+    }
+
+    // vérifie que les UE obligatoires/pré-requises ont été validées par l'étudiant
+    public Boolean verifierValidationDesUEObligatoires(ArrayList<UE> UEvalidees, ArrayList<UE> UEObligatoires) {
+
+        ArrayList<UE> tempList = new ArrayList<>(UEObligatoires);
+        while (!tempList.isEmpty()) {
+            if (UEvalidees.contains(tempList.getFirst()))
+                tempList.removeFirst();
+            else
+                return false;
+        }
+        return true;
+    }
+
+    public Boolean verifierValidationPrerequis(UE ue, Etudiant etudiant) {
+
+        ArrayList<ResultatUE> resultats = etudiant.getResultatsUE();
+        ArrayList<UE> UEvalidees = new ArrayList<>();
+        remplirListeUEValidees(UEvalidees, resultats);
+
+        ArrayList<UE> UEprerequis = new ArrayList<>(ue.getUEprerequis());
+        return verifierValidationDesUEObligatoires(UEvalidees, UEprerequis);
+    }
+
+    public Boolean estDiplome(Etudiant etudiant) {
+
+        ArrayList<ResultatUE> resultats = etudiant.getResultatsUE();
+        ArrayList<UE> UEObligatoires = etudiant.getParcours().getUEObligatoire();
+        ArrayList<UE> UEvalidees = new ArrayList<>();
+
+        int totalEcts = remplirListeUEValidees(UEvalidees, resultats);
+        return ((totalEcts >= 180) && verifierValidationDesUEObligatoires(UEvalidees,
+                UEObligatoires));
+    }
+}
---
+++ src/main/java/fr/miage/projetjava/model/Mention.java
@@ -0,0 +1,20 @@
+package fr.miage.projetjava.model;
+
+public enum Mention {
+    MIASHS("MIASHS"),
+    INFORMATIQUE("Informatique"),
+    BIOLOGIE("Biologie"),
+    CHIMIE("Chimie"),
+    PHYSIQUE("Physique");
+
+    private String affichage;
+
+    private Mention(String affichage) {
+        this.affichage = affichage;
+    }
+
+    @Override
+    public String toString() {
+        return this.affichage;
+    }
+}
