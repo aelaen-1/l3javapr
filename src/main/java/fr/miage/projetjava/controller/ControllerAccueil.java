2bcd609383b43e54e2acb7074c4e374401db06c6
aloon <118343415+aelaen-1@users.noreply.github.com>
2026-02-16 11:29:42 +0100

    conflicts

--- src/main/java/fr/miage/projetjava/controller/ControllerAccueil.java
+++ src/main/java/fr/miage/projetjava/controller/ControllerAccueil.java
@@ -1,4 +1,52 @@
-package fr.miage.projetjava.controller;
+package fr.miage.projetjava;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class ControllerAccueil {
+
+
+    @FXML
+    private Button monBouton;
+
+    @FXML
+    private void initialize()
+    {
+        monBouton.setStyle("-fx-background-color: #bb38ff;");
+    }
+
+    @FXML
+    private void changementFenetre(ActionEvent event)
+    {
+        // Récupère la fenetre actuelle
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        try {
+
+            //Localisation du nouveau fichier fxml qui va être afficher
+            final URL url = getClass().getResource("/visualisationDonneeCSV.fxml");
+
+            // Charge le nouveau FXML
+            final FXMLLoader fxmlLoader = new FXMLLoader(url);
+            final AnchorPane root = (AnchorPane) fxmlLoader.load();
+
+            // Nouvelle scène
+            final Scene scene = new Scene(root, 1100, 800);
+            //remplacement de l'ancien contenu de la fenetre par le nouveau
+            stage.setScene(scene);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stage.setTitle("Liste Étudiants");
+        stage.show();
+    }
 }
--- src/main/java/fr/miage/projetjava/controller/ControllerVisualisationDonneeCSV.java
+++ src/main/java/fr/miage/projetjava/controller/ControllerVisualisationDonneeCSV.java
@@ -1,4 +1,37 @@
-package fr.miage.projetjava.controller;
+package fr.miage.projetjava;
+import java.util.List;
+
+import javax.swing.text.html.ListView;
+
+import fr.miage.projetjava.model.Etudiant;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+
 
 public class ControllerVisualisationDonneeCSV {
+    //le nom (listeEtudiants) doit être le même que le fx:id de ListView<Etudiant> du fichier fxml (visualisationDonneeCSV)
+    @FXML private ListView<Etudiant> listeEtudiants;
+
+    //executé avant que la fenêtre ne soit affiché à l'écran mais après que les composants existent en mémoire, elle sert à préparer l'interface
+    //sert à initialiser les composants (avec des new), ajouter des listeners, configurer l'état intial de certains
+    // éléments, configrer certains éléments de TableView ou ListView etc....
+    @FXML
+    public void initialize()
+    {
+
+        // Charge et affiche les données CSV
+        ChargementCSV donneCSV = new ChargementCSV();
+
+        //on va appeler la méthode chargementFichierCSV qui va appeler les deux méthodes pour lire et récuperer les données du fichier csv
+        // et qui va créer les objets étudiants pour les mettre dans une liste
+        List<Etudiant> listeEtudiant = donneCSV.chargementFichierCSV();
+
+        //ObservableList est une interface de javafx, du même style que List mais ListView (ou TableView) peut voir si ya des
+        // changements et les prendre en compte
+        ObservableList<Etudiant> listeObservableEtudiant = FXCollections.observableList(listeEtudiant);
+
+        //va mettre les informations de listeEtudiants (transformer en liste observable) dans la ListView du fichier fxml
+        listeEtudiants.setItems(listeObservableEtudiant);
+    }
 }
