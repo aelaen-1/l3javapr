e3505e642025ae8b1032c503e5bec9b8decc5761
aloon <118343415+aelaen-1@users.noreply.github.com>
2026-02-16 09:53:39 +0100

    ajout fichiers controller

--- src/main/java/fr/miage/projetjava/controller/ControllerAccueil.java
+++ src/main/java/fr/miage/projetjava/controller/ControllerAccueil.java
@@ -1,15 +1,16 @@
 package fr.miage.projetjava;
 
-import java.io.IOException;
-import java.net.URL;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class ControllerAccueil {
 
--- src/main/java/fr/miage/projetjava/controller/ControllerVisualisationDonneeCSV.java
+++ src/main/java/fr/miage/projetjava/controller/ControllerVisualisationDonneeCSV.java
@@ -1,12 +1,9 @@
 package fr.miage.projetjava;
-import java.util.List;
-
-import javax.swing.text.html.ListView;
-
-import fr.miage.projetjava.model.Etudiant;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import java.util.List;
 
 
 public class ControllerVisualisationDonneeCSV {
