be9604db070e736eada7a27919522a2cb9d0bbfd
aloon <118343415+aelaen-1@users.noreply.github.com>
2026-02-16 11:37:25 +0100

    controller/*.java format + package

--- src/main/java/fr/miage/projetjava/controller/ChargementCSV.java
+++ src/main/java/fr/miage/projetjava/controller/ChargementCSV.java
@@ -1,7 +1,7 @@
 
-package fr.miage.projetjava;
+package fr.miage.projetjava.controller;
 
-
+import static java.lang.Integer.parseInt;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -10,36 +10,35 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static java.lang.Integer.parseInt;
+import fr.miage.projetjava.model.Etudiant;
 
 public class ChargementCSV {
 
-    //Va lire le fichier csv et récupérer ligne par ligne les information du fichier pour les stocker dans une liste de liste de String
-    private List<List<String>> lectureStockInfoCSV (BufferedReader br)
-    {
+    // Va lire le fichier csv et récupérer ligne par ligne les information du
+    // fichier pour les stocker dans une liste de liste de String
+    private List<List<String>> lectureStockInfoCSV(BufferedReader br) {
         // va stocker les différentes lignes du fichier csv
         List<List<String>> donneeCSV = new ArrayList<>();
 
-        //utiliser pour quand on va lire ligne par ligne le fichier csv qui est dans le buffer br
+        // utiliser pour quand on va lire ligne par ligne le fichier csv qui est dans le
+        // buffer br
         String line;
 
-        try
-        {
-            while ((line = br.readLine()) != null)
-            {
-                //line = 1,Prise,Bracelet,IO -> on stocke cette ligne dans un tableau de String et sépare les mots à
-                // dans ce tableau chaque virgule qu'il y a dans line pour que valeur donne ["1","Prise","Bracelet","IO"]
+        try {
+            while ((line = br.readLine()) != null) {
+                // line = 1,Prise,Bracelet,IO -> on stocke cette ligne dans un tableau de String
+                // et sépare les mots à
+                // dans ce tableau chaque virgule qu'il y a dans line pour que valeur donne
+                // ["1","Prise","Bracelet","IO"]
                 String[] valeur = line.split(",");
 
-                //transforme le tableau de String en une liste de String
+                // transforme le tableau de String en une liste de String
                 List<String> lineData = Arrays.asList(valeur);
 
-                //ajout de la ligne de csv dans donneeCSV
+                // ajout de la ligne de csv dans donneeCSV
                 donneeCSV.add(lineData);
             }
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             System.err.println("Error reading the CSV file: " + e.getMessage());
             e.printStackTrace();
         }
@@ -47,79 +46,71 @@ public class ChargementCSV {
         return donneeCSV;
     }
 
-
-    private Parcours creerParcours(String nomParcours)
-    {
+    private Parcours creerParcours(String nomParcours) {
         Parcours parcours;
-        if (nomParcours == "IO" || nomParcours == "MIAGE")
-        {
+        if (nomParcours == "IO" || nomParcours == "MIAGE") {
             parcours = new Parcours(nomParcours, Mention.MIASHS);
-        }
-        else if(nomParcours == "Biologie")
-        {
+        } else if (nomParcours == "Biologie") {
             parcours = new Parcours(nomParcours, Mention.BIOLOGIE);
-        }
-        else if(nomParcours == "Chimie")
-        {
+        } else if (nomParcours == "Chimie") {
             parcours = new Parcours(nomParcours, Mention.CHIMIE);
-        }
-        else if(nomParcours == "Informatique")
-        {
+        } else if (nomParcours == "Informatique") {
             parcours = new Parcours(nomParcours, Mention.INFORMATIQUE);
-        }
-        else
-        {
+        } else {
             parcours = new Parcours(nomParcours, Mention.PHYSIQUE);
         }
         return parcours;
     }
 
-    //avec la liste de liste de données du fichier CSV, la méthode va créer un à un tous les étudiants et les stocker dans une liste d'étudiant
-    private List<Etudiant> creationEtudiant(List<List<String>> data)
-    {
+    // avec la liste de liste de données du fichier CSV, la méthode va créer un à un
+    // tous les étudiants et les stocker dans une liste d'étudiant
+    private List<Etudiant> creationEtudiant(List<List<String>> data) {
         List<Etudiant> listeEtudiants = new ArrayList<>();
 
-        for (int i = 0; i < data.size()-1; i++) {
-            //récupération des différentes informations qui viennent du fichier CSV et qui sont nécessaire à la création des objets étudiants
-            int id = parseInt(data.get(i+1).get(0));
-            String prenomE = data.get(i+1).get(1);
-            String nomE = data.get(i+1).get(2);
-            String nomParcours = data.get(i+1).get(3);
+        for (int i = 0; i < data.size() - 1; i++) {
+            // récupération des différentes informations qui viennent du fichier CSV et qui
+            // sont nécessaire à la création des objets étudiants
+            int id = parseInt(data.get(i + 1).get(0));
+            String prenomE = data.get(i + 1).get(1);
+            String nomE = data.get(i + 1).get(2);
+            String nomParcours = data.get(i + 1).get(3);
 
-            //création de l'objet parcours de l'étudiant
+            // création de l'objet parcours de l'étudiant
             Parcours parcours = creerParcours(nomParcours);
 
+            // création de l'étudiant
+            Etudiant e = new Etudiant(id, nomE, prenomE, parcours);
 
-            //création de l'étudiant
-            Etudiant e = new Etudiant(id, nomE, prenomE, parcours );
-
-            //ajout de l'étudiant à la liste d'étudiant
+            // ajout de l'étudiant à la liste d'étudiant
             listeEtudiants.add(e);
 
         }
         return listeEtudiants;
     }
 
+    // va charger le ficher CSV, appeller la méthode pour lire et stocker les
+    // informations du fichier csv
+    // puis utiliser ce qui est retourné par cette méthode pour appeler la méthode
+    // pour créer la liste d'étudiant
+    // Cette méthode va être appelé dans ControllerVisualisationDonneeCSV
+    public List<Etudiant> chargementFichierCSV() {
 
-    //va charger le ficher CSV, appeller la méthode pour lire et stocker les informations du fichier csv
-    // puis utiliser ce qui est retourné par cette méthode pour appeler la méthode pour créer la liste d'étudiant
-    //Cette méthode va être appelé dans ControllerVisualisationDonneeCSV
-    public List<Etudiant> chargementFichierCSV()
-    {
-
-        // Chemin vers le fichier CSV, on pourras le mettre en paramètre de la méthode aussi
+        // Chemin vers le fichier CSV, on pourras le mettre en paramètre de la méthode
+        // aussi
         String fichierCSV = "src/main/resources/etudiant.csv";
 
-        // va stocker les différentes lignes du fichier csv lors de l'appel de la méthode lectureStockInfoCSV
+        // va stocker les différentes lignes du fichier csv lors de l'appel de la
+        // méthode lectureStockInfoCSV
         List<List<String>> donneeCSV = new ArrayList<>();
 
-        //va stocker les étudiants qui vont être créer à partir du fichier csv et de l'appel de la méthode creationEtudiant
+        // va stocker les étudiants qui vont être créer à partir du fichier csv et de
+        // l'appel de la méthode creationEtudiant
         List<Etudiant> listeEtudiants = new ArrayList<>();
 
-        //BufferedReader va permettre de lire ligne par ligne le fichier CSV
+        // BufferedReader va permettre de lire ligne par ligne le fichier CSV
         try (BufferedReader br = new BufferedReader(new FileReader(fichierCSV))) {
 
-            donneeCSV = lectureStockInfoCSV (br);
+            donneeCSV = lectureStockInfoCSV(br);
             listeEtudiants = creationEtudiant(donneeCSV);
 
         } catch (IOException e) {
@@ -131,4 +122,3 @@ public class ChargementCSV {
         return listeEtudiants;
     }
 }
-
--- src/main/java/fr/miage/projetjava/controller/ControllerAccueil.java
+++ src/main/java/fr/miage/projetjava/controller/ControllerAccueil.java
@@ -1,37 +1,35 @@
-package fr.miage.projetjava;
+package fr.miage.projetjava.controller;
 
-import javafx.fxml.FXML;
+import java.io.IOException;
+import java.net.URL;
+
+import org.w3c.dom.Node;
+
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.net.URL;
-
 public class ControllerAccueil {
-
 
     @FXML
     private Button monBouton;
 
     @FXML
-    private void initialize()
-    {
+    private void initialize() {
         monBouton.setStyle("-fx-background-color: #bb38ff;");
     }
 
     @FXML
-    private void changementFenetre(ActionEvent event)
-    {
+    private void changementFenetre(ActionEvent event) {
         // Récupère la fenetre actuelle
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         try {
 
-            //Localisation du nouveau fichier fxml qui va être afficher
+            // Localisation du nouveau fichier fxml qui va être afficher
             final URL url = getClass().getResource("/visualisationDonneeCSV.fxml");
 
             // Charge le nouveau FXML
@@ -40,9 +38,8 @@ public class ControllerAccueil {
 
             // Nouvelle scène
             final Scene scene = new Scene(root, 1100, 800);
-            //remplacement de l'ancien contenu de la fenetre par le nouveau
+            // remplacement de l'ancien contenu de la fenetre par le nouveau
             stage.setScene(scene);
-
 
         } catch (IOException e) {
             e.printStackTrace();
