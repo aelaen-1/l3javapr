fefd5bbc19d2aac1e8294bba74a429ec14fa8e99
Anais <anaistab7@gmail.com>
2026-02-18 18:51:13 +0100

    ajout du fichier ue.csv

--- src/main/java/fr/miage/projetjava/CSV/UECSV.java
+++ src/main/java/fr/miage/projetjava/CSV/UECSV.java
@@ -36,9 +36,9 @@ public class UECSV {
         }
     }
 
-
-    //utiliser split(,) pour creer l'arraylist de string puis les passer en arraylist d'UE
-    private List<UE>  passageStringToArrayList(String stringUEprerequis, List<UE> listeUE)
+    //va transformer la ligne String de pré-requis en liste de String et va ensuite aller chercher cette UE dans la liste
+    // d'UE déjà existante pour aller l'ajouter dans l'ArrayList pré-requis de l'UE
+    private void  ajoutPreRequisUE(String stringUEprerequis, List<UE> listeUE, UE ue)
     {
         //va faire un tableau de String et chaque élément du tableau va correspondre à un element de la liste qui est
         // séparé par une virgule
@@ -46,20 +46,33 @@ public class UECSV {
         //valeur va donner = ["UE1", "UE2", "UE3"]
         String[] tabStringUEPrerequis = stringUEprerequis.split(",");
 
-        List<String> lineData = Arrays.asList(tabStringUEPrerequis);
+        //passage en ArrayList
+        List<String> listeStringUECSV = Arrays.asList(tabStringUEPrerequis);
 
-        List<UE> UEprerequis = new ArrayList<>();
 
-        //faire une boucle for qui va vérifier que chaque UE de lineData existe dans listeUE et si c'est le cas on prend
-        // l'UE de listeUE et on l'ajoute à la liste UEprerequis
+        //faire une boucle for qui va vérifier que chaque UE de listeStringUE existe dans listeUE et si c'est le cas on prend
+        // l'UE de listeUE et on l'ajoute à l'ArrayList qui possède ces pré-requis et qui est dans les paramètres
 
-        return UEprerequis;
+        //on fait le tour de toutes les UE de la liste de pre-requis du CSV
+        for (int i = 0; i < listeStringUECSV.size(); i++){
+
+            //on fait le tour de toutes les UE déjà existantes
+            for(int j = 0; j<listeUE.size(); j++){
+
+                //si on trouve l'UE pré-requis dans la liste d'UE déjà existante on fait appel à la méthode
+                // de la classe UE permettant d'ajouter les UE pré-requis dans l'ArrayList
+                if (listeUE.get(j).getIntitule() == listeStringUECSV.get(i)){
+                    ue.setUEprerequis(listeUE.get(j));
+                }
+            }
+        }
+
     }
 
-    //String code, String intitule, int credit,   mention, ArrayList<UE> UEprerequis = new ArrayList<>();
     //avec la liste de liste de données du fichier CSV, la méthode va créer un à un toutes les UE et les stocker dans une liste d'UE
     private List<UE> creationUE(List<List<String>> data)
     {
+        //liste d'UE
         List<UE> listeUE = new ArrayList<>();
 
         for (int i = 0; i < data.size()-1; i++) {
@@ -67,11 +80,20 @@ public class UECSV {
             String code = data.get(i+1).get(0);
             String intitule = data.get(i+1).get(1);
             int credit = parseInt(data.get(i+1).get(2));
+
+            //on récupère mention qui va être sous forme de String et la méthode va la passer en Mention
             Mention mention = passageStringToMention(data.get(i+1).get(3));
-            List<UE> UEprerequis = data.get(i+1).get(4);
+
+            //on récupère la ligne des pré-requis et on va après appelé la méthode permettant de créer la liste des UE pré-requis
+            String stringUEprerequis = data.get(i+1).get(4);
 
             //création de l'UE
-            UE ue = new UE(code, intitule, credit, mention, UEprerequis);
+            UE ue = new UE(code, intitule, credit, mention);
+
+            //méthode permettant de passer de la ligne contenant les pre-requis à l'ajout des UE déjà
+            // existante dans l'arrayList de UE
+            ajoutPreRequisUE(stringUEprerequis, listeUE, ue);
+
 
             //ajout de l'UE à la liste d'UE
             listeUE.add(ue);
