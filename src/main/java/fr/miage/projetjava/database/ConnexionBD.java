ba4d1f2ce6093ed4b79fcebf753f11baaf697dc3
Anais <anaistab7@gmail.com>
2026-02-20 13:33:12 +0100

    Ajout d'un try pour activer l'utilisation des clés étrangères

--- src/main/java/fr/miage/projetjava/database/ConnexionBD.java
+++ src/main/java/fr/miage/projetjava/database/ConnexionBD.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.Logger;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConnexionBD {
     //contient une String de connexion à la BD, si la base de donnée testBD.db n'existe pas elle seras créé automatiquement
@@ -18,6 +19,11 @@ public class ConnexionBD {
         try{
             //établit une connexion à la BD testBD.db
             Connection connexionBD = DriverManager.getConnection(URL);
+
+            //pour avoir la possibilité d'avoir des clés étrangères
+            try (Statement stmt = connexionBD.createStatement()) {
+                stmt.execute("PRAGMA foreign_keys = ON;");
+            }
             log.info("Connexion à la base de donnée réussi");
             return connexionBD;
         }
