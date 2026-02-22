9a432e525dffa30bd039c6ea1bb0d8203ce2b7d2
saleh terefi <salehterefi77@gmail.com>
2026-02-22 23:29:44 +0100

    commentaire methode ObtenirUesAccessibles

--- src/main/java/fr/miage/projetjava/metier/ScolariteService.java
+++ src/main/java/fr/miage/projetjava/metier/ScolariteService.java
@@ -1,41 +1,84 @@
 package fr.miage.projetjava.metier;
 import fr.miage.projetjava.model.*;
-
 import java.util.ArrayList;
+import java.util.List;
 
 public class ScolariteService {
-    public ArrayList<UE> obtenirUEAccessibles(Etudiant e, ArrayList<UE> toutesUE) {
+    /* cette méthode permet d'obtenir  les Ues accessibles par un etudiant
+     * elle prend en entrée un etduant et la liste des Ues du parcours dans le quel l'etudidant est inscrit
+     * si l'utilisateur veut voir les Ues accessibles par un etudiant, le controller doit faire appel  a cete methode */
+
+    public List<UE> obtenirUEAccessibles(Etudiant etudiant, List<UE> toutesLesUE) {
+        //on crée un tableaux des Ues pour stocker les Ues accessibmes par l'étudiant
         ArrayList<UE> accessibles = new ArrayList<>();
-        System.out.println(" UE accessibles pour " + e.getNomE() + " ---");
 
-        for (UE ue : toutesUE) {
-            // Règle 1 : Même mention
-            boolean memeMention = ue.getMention().equals(e.getParcours().getMention());
-            // Règle 2 : Pas déjà inscrit
-            boolean dejaInscrit = e.getResultatsUE().stream()
-                    .anyMatch(res -> res.getUe().getCode().equals(ue.getCode()));
-            // Règle 3 : Prérequis OK
-            boolean prerequisOK = possedePrerequis(ue, e);
+        //on recupere les Ues du parcours de l'etudiant
+        List<UE> uesDuParcours = etudiant.getParcours().getUEObligatoire();
 
-            if (memeMention && !dejaInscrit && prerequisOK) {
-                accessibles.add(ue);
-                System.out.println("  [ACCESSIBLE] " + ue.getCode());
+        // On boucle sur les UE du parcours
+        for (UE ueDuParcours : uesDuParcours) {
+
+            // 1. ON VERIFIE LES PREREQUIS
+            // On part du principe que c'est bon, et on cherche si un prérequis manque
+            boolean possedePrerequis = true;
+            if (ueDuParcours.getUEprerequis() != null && !ueDuParcours.getUEprerequis().isEmpty()) {
+                for (UE prerequis : ueDuParcours.getUEprerequis()) {
+                    boolean prerequisValide = false;
+                    // On cherche dans les résultats de l'étudiant si le prérequis est validé
+                    for (ResultatUE res : etudiant.getResultatsUE()) {
+                        if (res.getUe().getCode().equals(prerequis.getCode()) && res.getStatut() == StatutUE.VALIDE) {
+                            prerequisValide = true;
+                            break;
+                        }
+                    }
+                    // Si un seul prérequis n'est pas validé, l'étudiant ne peut pas prendre l'UE
+                    if (!prerequisValide) {
+                        possedePrerequis = false;
+                        break;
+                    }
+                }
+            }
+            // Si l'étudiant a les prérequis, on regarde s'il peut s'inscrire
+            if (possedePrerequis) {
+                // On cherche si l'étudiant a déjà été inscrit à cette UE
+                ResultatUE historique = null;
+                for (ResultatUE res : etudiant.getResultatsUE()) {
+                    if (res.getUe().getCode().equals(ueDuParcours.getCode())) {
+                        historique = res;
+                        break;
+                    }
+                }
+                // si historique est vide on ajoute les Ues qui ont getPrerequis == null
+                if (historique == null) {
+                    if (ueDuParcours.getUEprerequis() == null || ueDuParcours.getUEprerequis().isEmpty()) {
+                        // on ajoute l'UE car elle n'a pas de prérequis et jamais été inscrite
+                        accessibles.add(ueDuParcours);
+                    }
+                }
+                // Sinon il a été inscrit au moins une fois
+                else {
+                    StatutUE statut = historique.getStatut();
+                    // Si c'est ECHOUE, il peut la reprendre mais au semestre suivant
+                    if (statut == StatutUE.ECHOUE) {
+                        if (!historique.getSemestre().equals(etudiant.getSemestreCourant())) {
+                            accessibles.add(ueDuParcours);
+                        }
+                    }
+                }
             }
         }
+        //on retourne le tableau des Ues accessibles
         return accessibles;
     }
-
     public boolean inscrireEtudiant(Etudiant e, UE ue, String annee, Semestre semestre) {
-        System.out.print("Inscription de " + e.getNomE() + " à " + ue.getCode() + "... ");
         if (!possedePrerequis(ue, e)) {
-            System.out.println("ÉCHEC (Prérequis manquants)");
             return false;
         }
         ResultatUE inscription = new ResultatUE(ue, annee, semestre, StatutUE.ENCOURS);
         e.getResultatsUE().add(inscription);
-        System.out.println("RÉUSSIE");
         return true;
     }
+
     public Boolean possedePrerequis(UE uecible, Etudiant e) {
         if (uecible.getUEprerequis() == null || uecible.getUEprerequis().isEmpty()) {
             return true;
@@ -43,18 +86,15 @@ public class ScolariteService {
         for (UE pre : uecible.getUEprerequis()) {
             boolean valide = false;
             for (ResultatUE res : e.getResultatsUE()) {
-                if (res.getUe().equals(pre) && res.getStatut() == StatutUE.VALIDE) {
+                if (res.getUe().getCode().equals(pre.getCode()) && res.getStatut() == StatutUE.VALIDE) {
                     valide = true;
                     break;
                 }
             }
-            if (!valide) {
-                return false; // un prérequis non validé → l'étudiant ne peut pas s'inscrire
-            }
+            if (!valide) return false;
         }
-        return true; // tous les prérequis validés
+        return true;
     }
-
 
     public Boolean estDiplome(Etudiant e) {
         int totalcredits = 0;
@@ -63,25 +103,24 @@ public class ScolariteService {
                 totalcredits += res.getUe().getCredit();
             }
         }
-        if (totalcredits < 180) {
-            return false;
-        }
+        if (totalcredits < 180) return false;
+
         for (UE ueObligatoire : e.getParcours().getUEObligatoire()) {
             boolean valide = false;
             for (ResultatUE res : e.getResultatsUE()) {
-                if (res.getUe().equals(ueObligatoire) && res.getStatut() == StatutUE.VALIDE) {
+                if (res.getUe().getCode().equals(ueObligatoire.getCode()) && res.getStatut() == StatutUE.VALIDE) {
                     valide = true;
+                    break;
                 }
             }
-            if (!valide) {
-                return false;
-            }
+            if (!valide) return false;
         }
         return true;
     }
+
     public boolean validerUE(Etudiant e, UE ue) {
         for (ResultatUE res : e.getResultatsUE()) {
-            if (res.getUe().equals(ue)) {
+            if (res.getUe().getCode().equals(ue.getCode())) {
                 res.setStatut(StatutUE.VALIDE);
                 return true;
             }
@@ -91,7 +130,7 @@ public class ScolariteService {
 
     public boolean echoueUE(Etudiant e, UE ue) {
         for (ResultatUE res : e.getResultatsUE()) {
-            if (res.getUe().equals(ue)) {
+            if (res.getUe().getCode().equals(ue.getCode())) {
                 res.setStatut(StatutUE.ECHOUE);
                 return true;
             }
@@ -104,6 +143,4 @@ public class ScolariteService {
         else
             e.setSemestreCourant(Semestre.IMPAIR);
     }
-
-
 }
