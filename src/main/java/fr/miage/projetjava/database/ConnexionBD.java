bbba239fed5d9d05422ca64c907118ff4fddc70d
Anais <anaistab7@gmail.com>
2026-03-20 21:54:43 +0100

    modification du code pour utiliser Oracle avec les identifiants dans un fichier

--- src/main/java/fr/miage/projetjava/database/ConnexionBD.java
+++ src/main/java/fr/miage/projetjava/database/ConnexionBD.java
@@ -2,12 +2,16 @@ package fr.miage.projetjava.database;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.lookup.PropertiesLookup;
 import org.apache.logging.log4j.core.lookup.RuntimeStrSubstitutor;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 /**
  * Classe ConnexionBD : Gère la connexion à la BD et la crée si elle n'existe pas
@@ -25,6 +29,81 @@ import java.sql.Statement;
 
 
 
+//Avec Oracle
+public class ConnexionBD {
+
+
+    private static final Logger log = LogManager.getLogger(ConnexionBD.class);
+
+
+    /**
+     * Elle permet d'établir une connexion à la Base de données présente dans URL.
+     * Elle est appelée depuis les classes dans le package dao (pour le moment BDEtudiant et BDParcours)
+     *
+
+     * @return : Une connexion à la BD de type Connection. Connection est une interface
+     *         Retourne une erreur si la connexion à la BD a échoué ou si le sta
+     */
+    public static Connection connexionBD()
+    {
+        //Properties est une classe java qui permet de stocker des paires clé/valeur et
+        //elle sait lire les fichiers .properties.
+        //Pour l'instant il est vide
+        Properties properties = new Properties();
+
+        //FileInputStream ouvre le fichier en lecture brute (octet par octet)
+        //try-with-ressources, java va fermer automatiquement le fichier à la fin du bloc
+        try(FileInputStream file = new FileInputStream("src/main/resources/config.properties")){
+            //Ici le fichier est vraiment lu et parsé. L'objet properties lit le fichier et
+            //transforme chaque ligne clé=valeur du fichier en entrée accessible en java
+            properties.load(file);
+
+            //db.URL contient une String de connexion à la BD, et getProperty permet de récupérer la valeur
+            //associée à la clé db.URL
+            //url = jdbc:oracle:thin:@adresse:port:SID
+            //jdbc est une API de bas niveau de Java et permet à java un accès à une BD
+            //c'est lui qui permet de se connecter et d'intéragir avec une BD
+            //jdbc permet d'utiliser la même API pour accéder aux BD grâce à l'utilisation de pilotes (drivers)
+            //oracle correspond au type de BD utilisé
+            //thin correspond au driver
+            //@adresse = nom du domaine du serveur où tourne Oracle
+            //port correspond au port du réseau
+            //SID nom de l'instance Oracle sur le serveur
+            String URL = properties.getProperty("db.URL");
+            String USER = properties.getProperty("db.USER");
+            String PASSWORD = properties.getProperty("db.PASSWORD");
+
+            // cette ligne permet d'établir une connexion avec la bd
+            //DriverManager est une classe donnant accès à plusieurs services permettant de gérer plusieurs drivers JDBC
+            //la méthode statique getConnection de DriverManager, renvoie un objet de type connection et
+            // permet la connexion à la BD
+            //Connection est une interface qui va permettre à connexionBD de représenter le canal ouvert
+            // entre la bd et le code, tant que connexionBD existe et est ouvert alors on peut envoyer des
+            // requêtes sql
+            Connection connexionBD = DriverManager.getConnection(URL, USER, PASSWORD);
+
+            log.info("Connexion à la base de donnée réussi");
+            return connexionBD;
+        }
+        // le RuntimException permet d'arrêter le programme lorsqu'il y a une erreur qui n'est pas géré
+        catch (IOException e){
+            log.error(e.getMessage());
+            //Exception que java n'oblige pas à gérer, RuntimeException est la classe mère de toutes les exceptions non vérifiées
+            throw new RuntimeException("Problème dans le chargement de config.properties");
+        }
+        catch(SQLException e){
+            log.error(e.getMessage());
+            //DriverManager.getConnection() peut lancer une SQLException mais connexionBD doit renvoyer une Connection,
+            // on enveloppe donc l'erreur dans une RuntimeExcpetion pour la propager
+            throw new RuntimeException("Erreur de connexion à la base de donnée");
+        }
+
+    }
+}
+
+
+//Avec SQLite
+/*
 public class ConnexionBD {
     //contient une String de connexion à la BD, si la base de donnée testBD.db n'existe pas elle seras créé automatiquement,
     //static car la base de données est la même pour tous et ca permet de ne pas avoir à instancier d'objet pour pouvoir faire une connexion à la BD
@@ -39,14 +118,14 @@ public class ConnexionBD {
     private static final Logger log = LogManager.getLogger(ConnexionBD.class);
 
 
-    /**
+    /
      * Elle permet d'établir une connexion à la Base de données présente dans URL.
      * Elle est appelée depuis les classes dans le package dao (pour le moment BDEtudiant et BDParcours)
      *
 
      * @return : Une connexion à la BD de type Connection. Connection est une interface
      *         Retourne une erreur si la connexion à la BD a échoué ou si le sta
-     */
+     /
     public static Connection connexionBD()
     {
         try{
@@ -78,3 +157,4 @@ public class ConnexionBD {
 
     }
 }
+*/
