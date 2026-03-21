0c73251bc662e1da7031c053b2e8f23318b4836e
Anais <anaistab7@gmail.com>
2026-03-21 16:28:49 +0100

    classe permettant d'ajouter les UE obligatoires d'un parcours dans la BD

--- src/main/java/fr/miage/projetjava/dao/UEObligatoiresBD.java
+++ src/main/java/fr/miage/projetjava/dao/UEObligatoiresBD.java
@@ -1,4 +1,95 @@
 package fr.miage.projetjava.dao;
 
+
+import fr.miage.projetjava.model.Mention;
+import fr.miage.projetjava.model.Parcours;
+import fr.miage.projetjava.model.UE;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * Classe UEObligatoires : Gère l'insertion des UE obligatoires liés à un parcours dans la BD
+ * 2 méthodes : static void insertUEObligatoires et static void recuperationInformationListUEObli
+ *
+ * Cette classe permet de récupérer la liste de parcours fabriquer à partie du fichier csv dans ParcoursDAO et
+ *  d'insérer les différents étudiants dans la BD
+ *
+ * Si l'insertion dans la base de données a échoué alors un message d'erreur est renvoyé
+ *
+ *
+ *
+ */
 public class UEObligatoiresBD {
+
+    private static final Logger log = LogManager.getLogger(UEObligatoiresBD.class);
+
+
+    /**
+     * Méthode appelée dans recuperationInformationListUEObli
+     *
+     * Cette méthode va insérer les UE obligatoire pour dans un parcours dans la base de donnée à partir des
+     * informations de la liste d'UE obligatoire donné en paramètre
+     *
+     * Si l'insertion dans la base de données a échoué alors un message d'erreur est renvoyé
+     *
+     * @param : String nomParcours, ArrayList<UE>: information nécessaire pour l'ajout des UE obligatoires dans la BD
+     *
+     */
+    public static void insertUEObligatoires(String nomParcours, ArrayList<UE> ueObligatoire)
+    {
+
+        // on se connecte à la bd et statement va être utilisé pour exécuter les requêtes SQL
+        //Statement est une classe permettant l'exécution des différentes requêtes avec l'appel de la méthode execute
+        //qui lui appartient. D'abord createStatement créé un objet Statement qui va ensuite pouvoir envoyer les requêtes sql
+        // à la BD
+        try(Connection connexion = ConnexionBD.connexionBD()){
+
+            //on va parcourir la liste des UE obligatoire du parcours et les insérer dans la table
+            for (UE ue : ueObligatoire){
+                String requeteInsertParcours = "INSERT INTO  UEObligatoire (nomParcours, UE)" +
+                        "VALUES ( ?, ?)";
+
+
+                PreparedStatement ajoutValues = connexion.prepareStatement(requeteInsertParcours);
+
+                ajoutValues.setString(1,nomParcours);
+                ajoutValues.setString(2,ue.getCode());
+
+                ajoutValues.executeUpdate();
+                log.info("parcours " + nomParcours + "ue: " +  ue.getCode()  +" ajouté ");
+            }
+        }
+        catch(SQLException e){
+            log.error(e.getMessage());
+        }
+    }
+
+
+    /**
+     * Méthode appelée depuis InsertBD dans la méthode insertionDonneeBD
+     *
+     * Cette méthode va récupérer les informations des parcours donné dans la liste de parcours
+     * et va appeler pour chaque parcours la méthode insertUEObligatoires
+     * afin d'insérer dans la table UEObligatoire toutes les UES obligatoire liés à un parcours
+     *
+     *
+     * @param : ArrayList<Parcours> listeParcoursCSV:  liste d'objets Parcours créés à partir des données du CSV
+     *
+     *
+     */
+    public static void recuperationInformationListUEObli(ArrayList<Parcours> listeParcoursCSV)
+    {
+        for (Parcours parcours : listeParcoursCSV) {
+            ArrayList<UE> ueObligatoire = parcours.getUEObligatoire();
+            String nomParcours = parcours.getNom();
+
+            insertUEObligatoires(nomParcours, ueObligatoire);
+        }
+    }
+
 }
