1187731eb09bf283c32989af46202caf76663446
Anais <anaistab7@gmail.com>
2026-03-23 18:52:37 +0100

    méthode permettant de récupérer e de renvoyer la liste d'UE obliatoire à un parcours ou prérequis à une autre UE

--- src/main/java/fr/miage/projetjava/dao/RecupererBD.java
+++ src/main/java/fr/miage/projetjava/dao/RecupererBD.java
@@ -1,4 +1,103 @@
 package fr.miage.projetjava.dao;
 
+import fr.miage.projetjava.model.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+
+
+/**
+ * Classe RecupererBD : Gère le fait de récupérer les différentes informations de la BD et de les stocker dans des listes.
+ * Ces listes vont ensuite être utilisées pour l'affichege des informations sur l'interface.
+ *
+ */
+
 public class RecupererBD {
+
+
+    private static final Logger log = LogManager.getLogger(RecupererBD.class);
+
+    /***
+     * Méthode appelé dans
+     *
+     * Cette méthode va chercher toutes les UE prérequis ou obligatoires d'une UE ou d'un parcours passé en paramètre
+     * et va renvoyer une ArrayList de ces UE prérequis ou obligatoires.
+     * @param : String nom, Connection connexion, ArrayList<UE> listeUE, String type:
+     * nom : indique soit le code de l'UE sur laquelle on veut récupérer les prérequis, soit le nom du parcours sur lequel
+     * on veut récupérer les UE obligatoires
+     * listeUE : indique la liste d'UE déjà existante dans laquelle on va aller chercher les UE obligatoires ou prérequis
+     * type : indique si c'est pour un parcours ou une UE
+     *
+     * @return la liste d'UE prérequis ou obligatoire récupérée de la BD
+     */
+
+    public static ArrayList<UE> recupInfoUEPrerequisObliBD(String nom, Connection connexion, ArrayList<UE> listeUE, String type){
+        ArrayList<UE> listeUEPrerequisObli = new ArrayList<>();
+        String requeteUE;
+
+        if(type.equals("prerequis")) {
+            //requête pour récupérer toutes les UE prérequis de l'UE que l'on veut ajouter dans l'ArrayList
+            requeteUE = "Select * From UEprerequis where codeUE = ?";
+        }
+        else {
+            //requête pour récupérer toutes les UE obligatoire du parcours que l'on veut ajouter dans l'ArrayList
+            requeteUE = "Select * From UEObligatoire where nomParcours = ?" ;
+        }
+
+
+        try{
+            //va nous permettre d'exécuter la requête
+            PreparedStatement recupUEPrerequisObli = connexion.prepareStatement(requeteUE);
+
+            recupUEPrerequisObli.setString(1, nom);
+
+            //pour récupérer les informations, true si ça renvoie des lignes et false si ca renvoie un nombre de ligne
+            boolean resultatRequeteUEPrerequisObli = recupUEPrerequisObli.execute();
+
+            //true, si on a bien ce qu'on veut, donc les informations concernant les UEPrerequis
+            if(resultatRequeteUEPrerequisObli){
+
+                //récupération des UE, ResultSet est un curseur qui va passer un à un sur les différentes lignes de ce qui a été renvoyé
+                ResultSet rsListeUEPrerequisObli = recupUEPrerequisObli.getResultSet();
+
+                //on passe ligne par ligne sur les différentes UE prerequis pour les ajouter à la liste
+                while(rsListeUEPrerequisObli.next()){
+
+                    //on va récupérer le code de l'UE obliatoire ou prérequis
+                    String codeUEprerequisObli;
+                    if(type.equals("prerequis")){
+                        codeUEprerequisObli = rsListeUEPrerequisObli.getString("codeUEPrerequis");
+
+                    }
+                    else {
+                        codeUEprerequisObli = rsListeUEPrerequisObli.getString("UE");
+
+                    }
+
+                    //les UE prérequis ou obligatoires sont des UE qui existent déjà on donc aller chercher dans la liste d'UE déjà
+                    // existante et on va l'ajouter à la liste d'UE prérequis ou obligatoire
+                    for (UE ueliste : listeUE){
+                        //on compare les UE existantes avec le code UE de l'UE prérequis ou obligatoire que l'on a récupéré
+                        if(ueliste.getCode().equals(codeUEprerequisObli)){
+                            listeUEPrerequisObli.add(ueliste);
+                            break;
+                        }
+                    }
+
+                }
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        return listeUEPrerequisObli;
+    }
+
+
+
+
 }
