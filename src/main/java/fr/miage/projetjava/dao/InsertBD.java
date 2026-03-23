3ae2f38b181f4bf642f65baff330dd9e5732cfa6
Anais <anaistab7@gmail.com>
2026-03-23 22:04:42 +0100

    ajout de la connexion à la bd en paramètre de la méthode qui appelle les autres méthodes d'insertion des données

--- src/main/java/fr/miage/projetjava/dao/InsertBD.java
+++ src/main/java/fr/miage/projetjava/dao/InsertBD.java
@@ -4,6 +4,7 @@ import fr.miage.projetjava.model.Etudiant;
 import fr.miage.projetjava.model.Parcours;
 import fr.miage.projetjava.model.UE;
 
+import java.sql.Connection;
 import java.util.ArrayList;
 
 
@@ -23,33 +24,36 @@ public class InsertBD {
      * Elle va d'abord charger une liste pour un fichier csv afin de récupérer les différentes informations de ces fichiers.
      * Elle va ensuite utiliser ces listes comme paramètre pour l'insertion des données dans la BD.
      *
+     * @param : Connection connexion
+     * connexion : connexion à la bd qui va être donné en appel des méthodes d'insertions des données dans la BD et qui
+     * va permettre d'exécuter les requêtes sql.
      *
      */
 
-    public static void insertionDonneeBD(){
+    public static void insertionDonneeBD(Connection connexion){
 
         //on charge les informations des UES du fichier csv dans une liste d'objets UE
         ArrayList<UE> listeUE = new UEDAO().chargerUEs();
         //Une fois toute les données du fichier csv chargées dans l'ArrayList on appel la méthode pour insérer les UE dans la BD
-        UEBD.recuperationInformationListUE(listeUE);
+        UEBD.recuperationInformationListUE(connexion, listeUE);
 
         //Pour les prérequis des UE on utilise la liste d'UE car c'est elle qui contient les objets UE et leurs prérequis
-        UEPrerequisBD.recuperationInformationListUEPre(listeUE);
+        UEPrerequisBD.recuperationInformationListUEPre(connexion, listeUE);
 
         //on charge les parcours du fichier csv dans une liste d'objets parcours
         ArrayList<Parcours> listeParcours = new ParcoursDAO().chargerParcours(listeUE);
         //Une fois toute les données du fichier csv chargées dans l'ArrayList on appel la méthode pour insérer les parcours dans la BD
-        ParcoursBD.recuperationInformationListParcours(listeParcours);
+        ParcoursBD.recuperationInformationListParcours(connexion, listeParcours);
 
         //Pour les UE obligatoireson on utilise listeParcours car c'est cette liste qui va contenir les UE obligatoires d'un parcours
-        UEObligatoiresBD.recuperationInformationListUEObli(listeParcours);
+        UEObligatoiresBD.recuperationInformationListUEObli(connexion, listeParcours);
 
         //on charge les étudiants du fichier csv dans une liste d'objets étudiants
         ArrayList<Etudiant> listeEtudiants = new EtudiantDAO().chargerTout(listeParcours, listeUE);
         //Une fois toute les données du fichier csv chargées dans l'ArrayList on appel la méthode pour insérer les étudiants dans la BD
-        EtudiantBD.recuperationInformationListEtudiant(listeEtudiants);
+        EtudiantBD.recuperationInformationListEtudiant(connexion, listeEtudiants);
 
         //Pour résultat UE on utilise la liste des étudiants car c'est dans celle-ci qu'est contenu les différents résultats des étudiants
-        ResultatUEBD.recuperationInformationListResultatUE(listeEtudiants);
+        ResultatUEBD.recuperationInformationListResultatUE(connexion, listeEtudiants);
     }
 }
